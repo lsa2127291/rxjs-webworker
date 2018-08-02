35160b56dc6ce250a57fe2c637b14743378323c4
luxin <704991434@qq.com>
2018-08-02 23:53:29 +0800

    add min.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,41 @@ const clean = require('gulp-clean')
 const babel = require('rollup-plugin-babel')
 const rollup = require('gulp-rollup')
 const resolve = require('rollup-plugin-node-resolve')
-
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 gulp.task('clean', () => {
   return gulp.src('./dist', {read: false}).pipe(clean())
 })
-gulp.task('build', ['clean'], () => {
+gulp.task('build-min', ['clean'], () => {
   return gulp.src('./src/**/*.js').pipe(rollup({
     allowRealFiles: true,
     input: './src/index.js',
     plugins: [resolve({
       module: true
     }), babel({
-      "presets": ["es2015-rollup"],
-    })],  
+      "presets": ["es2015-rollup"]
+    })],
+    output: {
+      format: 'umd',
+      name: 'RxWorker'
+    }
+  })).pipe(rename({
+    suffix: '.min'
+  })).pipe(uglify()).pipe(gulp.dest('dist'))
+})
+gulp.task('build-normal', ['clean'], () => {
+  return gulp.src('./src/**/*.js').pipe(rollup({
+    allowRealFiles: true,
+    input: './src/index.js',
+    plugins: [resolve({
+      module: true
+    }), babel({
+      "presets": ["es2015-rollup"]
+    })],
     output: {
       format: 'umd',
       name: 'RxWorker'
     }
   })).pipe(gulp.dest('dist'))
 })
-
+gulp.task('build', ['build-min', 'build-normal'])
